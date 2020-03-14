5dce23237cc0164a0f23ebdfb48eb385c4638618
vetordev <you@example.com>
2020-03-14 17:17:54 -0300

    script, der, inserts/selects of database added

---
+++ SQL/Insert's e Select's-Mercado.sql
@@ -0,0 +1,55 @@
+use mercado;
+/*aqui vai ter uma função pra calcular preço lote + 50% (p ter o lucro)*/
+insert into produto value('1','Arroz','Agulhinha Tipo 1 Pacote 5kg','Prato fino','Mercearia','18.00');
+insert into produto value('2','Feijão','Carioca 1kg Pacote','Camil','Mercearia','6.00');
+insert into produto value('3','Requeijão','Cremoso 200g','Vigor','Mercearia','5.00');
+
+
+insert into lote value('1','1','2020-04-20','12.00','240');
+insert into lote value('2','1','2020-04-20','3.00','60');
+insert into lote value('3','1','2020-04-20','2.50','50');
+
+
+insert into estoque value('1','1');
+insert into estoque value('2','2');
+insert into estoque value('3','3');
+
+
+insert into gondola value('1','1','1','100');
+insert into gondola value('2','2','2','70');
+insert into gondola value('3','3','3','50');
+
+
+insert into venda value('1','54.00','2020-03-13');
+insert into venda value('2','32.00','2020-03-10');
+insert into venda value('3','90.00','2020-03-07');
+
+/*mais insert's pra facilitar*/
+insert into item_venda value('1','1','1','2');
+insert into item_venda value('2','1','2','3');
+insert into item_venda value('3','2','3','4');
+insert into item_venda value('4','2','2','2');
+insert into item_venda value('5','3','1','5');
+
+
+insert into caixa value('1','1', null); 
+insert into caixa value('2','2', null); 
+insert into caixa value('3','3', null); 
+
+
+insert into cofre value('1','1', null, null);
+insert into cofre value('2','2', null, null);
+insert into cofre value('3','3', null, null);
+
+
+insert into encomenda value('1','1', '20');
+
+select * from produto;
+select * from lote;
+select * from estoque;
+select * from gondola;
+select * from venda;
+select * from item_venda;
+select * from caixa;
+select * from cofre;
+select * from encomenda;
---
+++ SQL/Script-Mercado.sql
@@ -0,0 +1,185 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mercado` DEFAULT CHARACTER SET utf8 ;
+USE `mercado` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`produto`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`produto` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`produto` (
+  `id_produto` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `descricao` VARCHAR(60) NOT NULL,
+  `marca` VARCHAR(45) NOT NULL,
+  `setor` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_produto`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`lote`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`lote` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`lote` (
+  `id_lote` INT NOT NULL,
+  `id_produto` INT NOT NULL,
+  `validade_lote` DATE NOT NULL,
+  `preco_unidade` FLOAT NOT NULL,
+  `preco_lote` FLOAT NOT NULL,
+  PRIMARY KEY (`id_lote`),
+  CONSTRAINT `id_produtoLote`
+    FOREIGN KEY (`id_produto`)
+    REFERENCES `mercado`.`produto` (`id_produto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`estoque`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`estoque` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`estoque` (
+  `id_estoque` INT NOT NULL,
+  `id_lote` INT NOT NULL,
+  PRIMARY KEY (`id_estoque`),
+  CONSTRAINT `id_loteEstoque`
+    FOREIGN KEY (`id_lote`)
+    REFERENCES `mercado`.`lote` (`id_lote`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`gondola`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`gondola` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`gondola` (
+  `id_gondola` INT NOT NULL,
+  `id_estoque` INT NOT NULL,
+  `id_produto` INT NOT NULL,
+  `quantidade_produto` INT NOT NULL,
+  PRIMARY KEY (`id_gondola`),
+  CONSTRAINT `id_estoqueGondola`
+    FOREIGN KEY (`id_estoque`)
+    REFERENCES `mercado`.`estoque` (`id_estoque`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_produtoGondola`
+    FOREIGN KEY (`id_produto`)
+    REFERENCES `mercado`.`produto` (`id_produto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`venda`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`venda` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`venda` (
+  `id_venda` INT NOT NULL,
+  `preco_total` FLOAT NOT NULL,
+  `data_venda` DATE NOT NULL,
+  PRIMARY KEY (`id_venda`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`item_venda`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`item_venda` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`item_venda` (
+`id_item_venda` INT NOT NULL,
+`id_venda` INT NOT NULL,
+`id_produto` INT NOT NULL,
+`quantidade` INT NOT NULL,
+	PRIMARY KEY (`id_item_venda`),
+	CONSTRAINT `id_vendaItem`
+    FOREIGN KEY (`id_venda`)
+    REFERENCES `mercado`.`venda` (`id_venda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+	CONSTRAINT `id_itemVendaProduto`
+    FOREIGN KEY (`id_produto`)
+    REFERENCES `mercado`.`produto` (`id_produto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`caixa`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`caixa` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`caixa` (
+  `id_caixa` INT NOT NULL,
+  `id_venda` INT NOT NULL,
+  `lucro_diario` FLOAT,
+  PRIMARY KEY (`id_caixa`),
+  CONSTRAINT `id_vendaCaixa`
+    FOREIGN KEY (`id_venda`)
+    REFERENCES `mercado`.`venda` (`id_venda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cofre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`cofre` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`cofre` (
+  `id_cofre` INT NOT NULL,
+  `id_caixa` INT NOT NULL,
+  `lucro_semanal` FLOAT,
+  `lucro_mensal` FLOAT,
+  PRIMARY KEY (`id_cofre`),
+  CONSTRAINT `id_caixaCofre`
+    FOREIGN KEY (`id_caixa`)
+    REFERENCES `mercado`.`caixa` (`id_caixa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`encomenda`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mercado`.`encomenda` ;
+
+CREATE TABLE IF NOT EXISTS `mercado`.`encomenda` (
+  `id_encomenda` INT NOT NULL,
+  `id_lote` INT NOT NULL,
+  `quantidade_lote` INT NOT NULL,
+  PRIMARY KEY (`id_encomenda`),
+  CONSTRAINT `id_loteEncomenda`
+    FOREIGN KEY (`id_lote`)
+    REFERENCES `mercado`.`lote` (`id_lote`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
